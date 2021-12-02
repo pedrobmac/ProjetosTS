69809c29f2d8920bcabc4804e3b7078e165b5d87
Pedro Bittencourt <pbittenc@gmail.com>
2021-12-02 13:56:20 -0300

    projeto agenda - registro de usuário

--- secao11 - js-node-express-mongo/src/controllers/loginController.js
+++ secao11 - js-node-express-mongo/src/controllers/loginController.js
@@ -1,3 +1,31 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
     res.render('login');
+};
+
+exports.register = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.register();
+
+        //se tiver erro no cadastro, mostrar com mensagem flash e redirecionar de volta para a página de login
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                return res.redirect('/login');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso');
+        req.session.save(() => {
+            return res.redirect('/login');
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+
 };
