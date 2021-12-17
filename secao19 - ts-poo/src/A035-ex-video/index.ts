0cf7399362cfc8d28d56c711c324098fe75d9a85
Pedro Bittencourt <pbittenc@gmail.com>
2021-12-17 12:52:21 -0300

    exercicio interface e media

---
+++ secao19 - ts-poo/src/A035-ex-video/index.ts
@@ -0,0 +1,1 @@
+import './A035-ex-video';
--- secao19 - ts-poo/webpack.config.js
+++ secao19 - ts-poo/webpack.config.js
@@ -1,26 +1,27 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/A018-web-pack/index.ts",
+  mode: 'development',
+  // entry: "./src/A018-web-pack/index.ts",
+  entry: './src/A035-ex-video/index.ts',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
         options: {
-          configFile: "tsconfig.frontend.json",
+          configFile: 'tsconfig.frontend.json',
         },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "frontend", "assets", "js"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
   },
-  devtool: "source-map",
+  devtool: 'source-map',
 };
