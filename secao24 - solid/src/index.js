0e908e252cc32383bb341601aca1d59bf857e319
Pedro Bittencourt <pbittenc@gmail.com>
2022-01-12 18:30:31 -0300

    SOLID pros and cons

---
+++ secao24 - solid/src/index.js
@@ -0,0 +1,22 @@
+/*
+Vantagens e desvantagens do SOLID
+
+* Vantagens:
+- Código modular
+- Código Reutilizável (DRY - Don't Repeat Yourself)
+- Baixo acoplamento e alta coesão
+- Código testável e mock fácil
+- Fácil expansão no código (com menos chances de quebrar testes já existentes)
+- Separation of Concerns
+- Fácil manutenção
+
+* Desvantagens:
+- Complexidade da estrutura (e suas relações)
+- Quantdade de código
+- Cuidado para equilibrar com YAGNI e KISS (You aren't gonna need it; Keep It Simple, Stupid)
+- Tempo de desenvolvimento maior
+
+* Observações:
+- No começo, vale a pena começar a escrever o código e depois que tomar mais forma, refatorá-lo nos princípios do SOLID
+
+*/
